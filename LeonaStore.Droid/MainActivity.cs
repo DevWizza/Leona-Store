cf7f46fb2072c41e12c65b84128e62808b513b7d
Luis <luis.pena.nunez.developer@gmail.com>
2017-03-31 14:41:21 -0500

    Initial Commit

---
+++ LeonaStore.Droid/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Prism;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace LeonaStore.Droid
+{
+	[Activity(Label = "LeonaStore.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication(new App(new AndroidInitializer()));
+		}
+	}
+
+	public class AndroidInitializer : IPlatformInitializer
+	{
+		public void RegisterTypes(IUnityContainer container)
+		{
+
+		}
+	}
+}
