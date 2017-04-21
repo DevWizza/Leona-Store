f333d22efe8e07541bce989a54e219610ef3a981
Luis <luis.pena.nunez.developer@gmail.com>
2017-04-21 17:08:18 -0500

    Featuring Most important features

---
+++ LeonaStore/Components/ListingModelSelection/ListingModelSelectionTemplate.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ListingModelSelection
+{
+	public partial class ListingModelSelectionTemplate : StackLayout
+	{
+		public ListingModelSelectionTemplate()
+		{
+			InitializeComponent();
+		}
+	}
+}
