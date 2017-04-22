ab4d1a8e7950a9d4452d939661a3dacf681d3de1
Luis <luis.pena.nunez.developer@gmail.com>
2017-04-22 15:47:52 -0500

    Updated Gist

--- LeonaStore/Services/ListingServices/IListingService.cs
+++ LeonaStore/Services/ListingServices/IListingService.cs
@@ -8,10 +8,10 @@ namespace ListingServices
 {
 	public interface IListingService
 	{
-		[Get("/LuisAlbertoPenaNunez/26bd329c9f4a8205f50e417b0ace3564/raw/6be896a249f89628676c6df8488464ac1cb42fe4/listingItems.json")]
+		[Get("/LuisAlbertoPenaNunez/26bd329c9f4a8205f50e417b0ace3564/raw/d09966e6a2e5e7f4fda307f03fb4048cf29885fa/listingItems.json")]
 		Task<List<ListingItem>> GetAllListings();
 
-		[Get("/LuisAlbertoPenaNunez/26bd329c9f4a8205f50e417b0ace3564/raw/6be896a249f89628676c6df8488464ac1cb42fe4/listingItems.json")]
+		[Get("/LuisAlbertoPenaNunez/26bd329c9f4a8205f50e417b0ace3564/raw/d09966e6a2e5e7f4fda307f03fb4048cf29885fa/listingItems.json")]
 		Task<ListingItem> GetListing(string productId);
 	}
 }
