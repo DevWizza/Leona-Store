df639e87ba2380cbb40f32d4a82f190ba9c8e355
Luis <luis.pena.nunez.developer@gmail.com>
2017-04-23 20:51:37 -0500

    Search Layout

---
+++ LeonaStore/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeonaStore.Domain;
+using ListingServices;
+using Prism.Navigation;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LeonaStore.ViewModels
+{
+	public class SearchPageViewModel : BindableBase, INavigationAware
+	{
+		public IList<ListingItem> Articles { get; set; }
+
+		public ListingItem ItemSelected { get; set; }
+
+		public ICommand NavigateBackCommand { get; set; }
+
+		readonly IListingService _listingService;
+
+		readonly INavigationService _navigationService;
+
+		public SearchPageViewModel(IListingService listingService,
+		                           INavigationService navigationService)
+		{
+			_navigationService = navigationService;
+
+			_listingService = listingService;
+
+			NavigateBackCommand = new Command(OnNavigateBack);
+		}
+
+		async void OnNavigateBack()
+		{
+			await _navigationService.GoBackAsync(useModalNavigation: true);
+		}
+
+		async Task OnRefreshListing()
+		{
+			Articles = await _listingService.GetAllListings();
+		}
+
+		public void OnNavigatedFrom(NavigationParameters parameters)
+		{
+			
+		}
+
+		public async void OnNavigatedTo(NavigationParameters parameters)
+		{
+			await OnRefreshListing();
+		}
+
+		public void OnNavigatingTo(NavigationParameters parameters)
+		{
+			
+		}
+	}
+}
---
+++ LeonaStore/Views/SearchPage/SearchPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace LeonaStore.Views
+{
+	public partial class SearchPage : ContentPage
+	{
+		public SearchPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
